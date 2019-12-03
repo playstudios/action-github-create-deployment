f75833df5cd31fef33c6cd2ff6d9ccb3b4e03519
Jon Walton <jonw@playstudios.asia>
2019-12-03 16:18:16 +0800

    initial implementation

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,24 @@
 import * as core from '@actions/core';
-import {wait} from './wait'
+import * as github from '@actions/github';
+import { ReposCreateDeploymentParams } from '@octokit/rest'
 
 async function run() {
-  try {
-    const ms = core.getInput('milliseconds');
-    console.log(`Waiting ${ms} milliseconds ...`)
+    let octokit = new github.GitHub(process.env.GITHUB_TOKEN || '');
+    let deploymentResponse = await octokit.repos.createDeployment(<ReposCreateDeploymentParams>{
+        owner: core.getInput('owner'),
+        repo: process.env.GITHUB_REPOSITORY,
+        ref: core.getInput('ref'),
 
-    core.debug((new Date()).toTimeString())
-    await wait(parseInt(ms, 10));
-    core.debug((new Date()).toTimeString())
-
-    core.setOutput('time', new Date().toTimeString());
-  } catch (error) {
-    core.setFailed(error.message);
-  }
+        auto_merge: false,
+        required_contexts: <string[]>[],
+        environment: core.getInput('environment'),
+        payload: core.getInput('payload'),
+    });
+    if (deploymentResponse.status != 201) {
+        core.setFailed(`createDeployment failed with status core ${deploymentResponse.status}`)
+    }
 }
 
-run();
+if (process.env.GITHUB_ACTIONS) {
+    run().catch((e) => core.setFailed(e.toString()));
+}
